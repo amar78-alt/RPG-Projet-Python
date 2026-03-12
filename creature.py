afc80f9b26aadf5af1ec94a0ce784241d92f0656
Imane <imaneabedje.aminou@ecoles-epsi.net>
2026-03-12 17:26:42 +0100

    modification de creature.py

--- creature.py
+++ creature.py
@@ -1,3 +1,5 @@
+from des import lancer_d20
+
 class Creature:
     def __init__(self, nom, description, pv, defense, typeDegats):
         self.nom = nom                   
@@ -7,4 +9,25 @@ class Creature:
         self.typeDegats = typeDegats     
         self.initiative = 0              
         self.liste_actions = []          
-        self.etats = []                  
+        self.etats = []  
+
+# Lance un jet d'initiative (1d20) pour déterminer l'ordre de jeu.
+    def lancer_initiative(self):
+        self.initiative = lancer_d20()
+        print(f"{self.nom} lance l'initiative: {self.initiative}")
+        return self.initiative 
+    
+#Affiche toutes les caractéristiques de la créature.   
+def afficher_caracteristiques(self):
+        print("|---| " + self.nom + " |---|")
+        print("  Description: " + self.description)
+        print("  PV: " + str(self.pv) + " / " + str(self.pv_max))
+        print("  Défense: " + str(self.defense))
+        print("  Type de degats: " + str(self.type_degats))
+
+
+
+
+
+
+
