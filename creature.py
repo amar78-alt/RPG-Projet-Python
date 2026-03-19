3e78705b0c35b6c80fbd9e3bc975b76d7dad936b
Imane <imaneabedje.aminou@ecoles-epsi.net>
2026-03-19 15:03:59 +0100

    finition de la classe creature

--- creature.py
+++ creature.py
@@ -1,33 +1,92 @@
 from des import lancer_d20
 
-class Creature:
-    def __init__(self, nom, description, pv, defense, typeDegats):
-        self.nom = nom                   
-        self.description = description   
-        self.pv = pv                     
-        self.defense = defense           
-        self.typeDegats = typeDegats     
-        self.initiative = 0              
-        self.liste_actions = []          
-        self.etats = []  
 
-# Lance un jet d'initiative (1d20) pour déterminer l'ordre de jeu.
+class Creature:
+
+    def __init__(self, nom, description, pv, defense, typeDegats):
+        self.nom = nom
+        self.description = description
+        self.pv = pv
+        self.pv_max = pv
+        self.defense = defense
+        self.typeDegats = typeDegats
+        self.initiative = 0
+        self.liste_actions = []
+        self.etats = []
+        self.est_ko = False
+
+
+# Lance un jet d'initiative (1d20)
     def lancer_initiative(self):
         self.initiative = lancer_d20()
-        print(f"{self.nom} lance l'initiative: {self.initiative}")
-        return self.initiative 
-    
-#Affiche toutes les caractéristiques de la créature.   
-def afficher_caracteristiques(self):
+        print(self.nom + " lance l'initiative: " + str(self.initiative))
+        return self.initiative
+
+
+# Affiche les caractéristiques de la créature
+    def afficher_caracteristiques(self):
         print("|---| " + self.nom + " |---|")
-        print("  Description: " + self.description)
-        print("  PV: " + str(self.pv) + " / " + str(self.pv_max))
-        print("  Défense: " + str(self.defense))
-        print("  Type de degats: " + str(self.type_degats))
+        print("Description: " + self.description)
+        print("PV: " + str(self.pv) + " / " + str(self.pv_max))
+        print("Défense: " + str(self.defense))
+        print("Type de degats: " + self.typeDegats)
+
+        if len(self.etats) == 0:
+            print("Etats: Aucun")
+        else:
+            print("Etats:", self.etats)
 
 
+# Affiche les actions disponibles
+    def afficher_actions(self):
+        if len(self.liste_actions) == 0:
+            print("Aucune action disponible")
+        else:
+            print("Actions disponibles :")
+            for i, action in enumerate(self.liste_actions, 1):
+                print(str(i) + ". " + action)
 
 
+# Faire subir des dégâts
+    def subir_degats(self, degats):
+        self.pv = self.pv - degats
+        print(self.nom + " subit " + str(degats) + " degats")
 
+        if self.pv <= 0:
+            self.pv = 0
+            self.est_ko = True
+            print(self.nom + " est KO !")
+
+
+# Vérifier si la créature est vivante
+    def est_vivante(self):
+        if self.pv > 0:
+            return True
+        else:
+            return False
+
+
+# Ajouter un état 
+    def ajouter_etat(self, etat):
+        if etat not in self.etats:
+            self.etats.append(etat)
+            print(self.nom + " est maintenant " + etat)
+
+
+# Retirer un état
+    def retirer_etat(self, etat):
+        if etat in self.etats:
+            self.etats.remove(etat)
+            self.etats ="normal"
+            print(self.nom + " n'est plus " + etat)
+
+
+# Vérifie si la créature peut être attaquée
+    def attaquable(self):
+        if self.est_ko:
+            return False
+        if "paralysé" in self.etats:
+            return False
+        return True
 
 
